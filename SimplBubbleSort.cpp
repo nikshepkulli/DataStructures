1b1442949a9cc9074ad92a5520cd72fc582adf3d
Nikshep A Kulli <48430792+nikshepkulli@users.noreply.github.com>
2024-01-21 15:35:33 -0500

    Update SimplBubbleSort.cpp

--- SimplBubbleSort.cpp
+++ SimplBubbleSort.cpp
@@ -6,7 +6,7 @@ int a[]={8,7,6,5,2,4,1,3,9};
 int getArrayLength = sizeof(a) / sizeof(int);
 for (int i = 0; i < getArrayLength; i++)//up unitl the length
 {
-    for(int j = 0;j<getArrayLength-1; j++){//up until last but one
+    for(int j = 0;j<getArrayLength-1; j++){//up until last but one //-i for decreasing number of passes
         if(a[j]>a[j+1]){
             int temp = a[j];
             a[j] = a[j+1];
